776a3a2ed7b4112688d6875681cb27ee30714fc2
Julie S Mike <juliesmike@gmail.com>
2017-02-12 18:06:32 -0600

    Add function works

--- public/scripts/animal.service.js
+++ public/scripts/animal.service.js
@@ -1,38 +1,22 @@
 angular.module('animalApp')
   .service('animal', animalAPIService);
 
-function animalAPIService($http){
-// console.log('apiservice working');
+function animalAPIService($http, $httpParamSerializerJQLike){
 
-// var url = 'https://animalrestapi.azurewebsites.net/Animal';
-// var params = {candidateID: '68f78af7-391c-4321-82e5-31727460a2e1'};
-// var head = {'Content-Type': 'application/x-www-form-urlencoded'};
-// //
+var url = 'https://animalrestapi.azurewebsites.net/Animal';
+var params = {candidateID: '68f78af7-391c-4321-82e5-31727460a2e1'};
+var head = {'Content-Type': 'application/x-www-form-urlencoded'};
 //
-// this.getAnimals = function(){
-// var req = {
-//   method: 'GET',
-//   url: url + '/List',
-//   headers: head,
-//   params: params
-// }
-//     return $http(req)
-//       .then(function(response){
-//       console.log('getAnimals response', response);
-//       return response.data.list;
-//   });
-// }
-// }
-
 
 this.getAnimals = function(){
-  var req = {
-    method: 'GET',
-    url: 'https://animalrestapi.azurewebsites.net/Animal/List/',
-    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-    params: {candidateID: '68f78af7-391c-4321-82e5-31727460a2e1'}
+var req = {
+  method: 'GET',
+  url: url + '/List',
+  headers: head,
+  params: params
 }
-    return $http(req).then(function(response){
+    return $http(req)
+      .then(function(response){
       console.log('getAnimals response', response);
       return response.data.list;
   });
@@ -41,32 +25,31 @@ this.getAnimals = function(){
 this.getDetail = function(animalID){
   var req = {
     method: 'GET',
-    url: 'https://animalrestapi.azurewebsites.net/Animal/Id/' + animalID,
-    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-    params: {candidateID: '68f78af7-391c-4321-82e5-31727460a2e1'}
+    url: url +'/Id/' + animalID,
+    headers: head,
+    params: params
 }
     return $http(req).then(function(response){
-      // console.log('get detail response', response.data.animal);
       return response.data.animal;
   });
 };
 
-this.createNewAnimal = function(name){
+this.createNewAnimal = function(name, scientificName, family, imageUrl){
   var animalDetail = {
-    list: Array
-      [commonName = name
-      ]
-    }
+          commonName: name,
+          scientificName: scientificName,
+          family: family,
+          imageUrl: imageUrl};
+          console.log(animalDetail);
 
   var req = {
     method: 'POST',
-    url: 'https://animalrestapi.azurewebsites.net/Animal/Create',
-    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-    params: {candidateID: '68f78af7-391c-4321-82e5-31727460a2e1'},
-    data: animalDetail
+    url: url + '/Create',
+    headers: head,
+    params: params,
+    data: $httpParamSerializerJQLike(animalDetail)
 }
     return $http(req).then(function(response){
-      console.log('create response', response);
       return response;
   });
 };
--- public/scripts/main.controller.js
+++ public/scripts/main.controller.js
@@ -7,46 +7,57 @@ function MainController(animal) {
   main.animalID = [];
   animal.data = {};
   main.newAnimal = [];
-  console.log('animal', animal)
-  // console.log('main controller working');
 
   animal.getAnimals()
     .then(function(animalList){
-      console.log('animal list', animalList);
       main.animalList = animalList;
     });
-    // animal.getAnimals();
 
-main.getAnimalDetail = function(id){
+  main.getAnimalDetail = function(id){
     animal.getDetail(id)
     .then(function(animalInfo){
     main.info = animalInfo;
-    console.log('get animal detail info', main.info);
-
     });
   }
 
-    main.getThisID = function(e){
-      // console.log('getthisid works');
-      // console.log('id from getThisID', e);
-      // animal.getDetail(e);
-      main.getAnimalDetail(e);
-    }
+  main.getThisID = function(thisId){
+    main.getAnimalDetail(thisId);
+  }
 
 
-  main.createAnimal = function(name){
-    console.log('createAnimals working');
-    console.log('name', name);
-    // var animalData = {
-    //     commonName: name
-    //
-    // }
-    animal.createNewAnimal(name)
-      .then(function(response){
-        console.log('animalData', name)
+  main.createAnimal = function(name, scientificName, family, imageUrl){
+    animal.createNewAnimal(name, scientificName, family, imageUrl)
+      .then(function(){
+        animal.getAnimals();
+        name = '';
 
-        console.log('response in create animal function', response);
+      }, function(error){
+        console.log('error creating animal', error);
       });
-}
+  }
 
 }
+
+
+//directive to convert url to correct format
+// angular.module('animalApp')
+//     .directive('httpPrefix', function() {
+//         return {
+//             restrict: 'A',
+//             require: 'ngModel',
+//             link: function(scope, element, attrs, controller) {
+//                 function ensureHttpPrefix(value) {
+//                     // Add prefix if we don't have http:// prefix already AND we don't have part of it
+//                     if (value && !/^(https?):\/\//i.test(value) &&
+//                         'http://'.indexOf(value) !== 0 && 'https://'.indexOf(value) !== 0) {
+//                         main.$setViewValue('http://' + value);
+//                         main.$render();
+//                         return 'http://' + value;
+//                     } else
+//                         return value;
+//                 }
+//                 main.$formatters.push(ensureHttpPrefix);
+//                 main.$parsers.splice(0, 0, ensureHttpPrefix);
+//             }
+//         };
+//     });
